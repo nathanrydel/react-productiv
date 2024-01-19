318d0ecfb5b59f8842ea1941769c1a6ad6e48d33
Nathan Rydel <nathanrydel@gmail.com>
2024-01-18 18:47:33 -0700

    updated copyright year in footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,12 +4,12 @@ import React from 'react';
 
 function Footer() {
   return (
-      <footer className="App-footer mt-5 text-muted">
-        <small>
-          Prødutïv is copyright ©2020 by Flüffy Data Enterprises, Inc.
-        </small>
-      </footer>
-  )
+    <footer className="App-footer mt-5 text-muted">
+      <small>
+        Prødutïv is copyright ©{new Date().getFullYear()} by Flüffy Data Enterprises, Inc.
+      </small>
+    </footer>
+  );
 }
 
 export default Footer;
