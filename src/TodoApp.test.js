b070bad8542caabbcf10884f2b9063a688cdc94e
Nathan Rydel <nathanrydel@gmail.com>
2024-01-19 13:06:22 -0700

    added smoke test, successful render of Todos and Top Todo areas, added snapshot

---
+++ src/TodoApp.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import TodoApp from "./TodoApp";
+
+const TODOS = [
+  { id: 1, title: "Test1", description: "desc", priority: 1 },
+  { id: 2, title: "Test2", description: "desc2", priority: 3 },
+  { id: 3, title: "Test3", description: "desc3", priority: 2 },
+]
+
+// beforeEach()
+
+// TodoApp smoke Test
+describe("TodoApp smoke test", function () {
+  test("it renders without crashing", function () {
+    render(<TodoApp initialTodos={TODOS} />);
+  });
+});
+
+// Renders Todos area with 1 Todo Present and Top Todo with a Todo
+
+describe("Rendering TodoApp with content", function () {
+  test("Todos area renders with a todo", function () {
+    const result = render(<TodoApp initialTodos={TODOS} />)
+
+    expect(result.queryByText("Todos")).toBeInTheDocument();
+    expect(result.queryByText("Top Todo")).toBeInTheDocument();
+    expect(result.queryAllByText("Test1")).toHaveLength(2);
+    expect(result.queryByText("Test2")).toBeInTheDocument();
+    expect(result.queryByText("Test3")).toBeInTheDocument();
+  })
+  // Snapshot of rendered Todos
+  test("matches snapshot of correct result", function () {
+    const result = render(<TodoApp initialTodos={TODOS} />)
+
+    expect(result).toMatchSnapshot();
+  })
+})
+
+// Renders Todos area with message You have no todos and TopTodo with No todos yet!
+// Snapshot of No Todos
