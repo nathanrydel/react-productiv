dc2ddf5385405a7d3d99ae5268e82b247725ad4f
Carl Molina <carlandrew.molina@gmail.com>
2024-01-18 16:54:05 -0800

    auto formatted todoapp

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -16,11 +16,11 @@ import EditableTodoList from "./EditableTodoList";
  */
 
 function TodoApp({ inititalTodos }) {
-  const [ todos, setTodos ] = useState(inititalTodos);
+  const [todos, setTodos] = useState(inititalTodos);
 
   /** add a new todo to list */
   function create(newTodo) {
-    const todo = {...newTodo, id: uuid()};
+    const todo = { ...newTodo, id: uuid() };
     setTodos(tasks => [...tasks, todo]);
   }
 
@@ -37,42 +37,42 @@ function TodoApp({ inititalTodos }) {
   }
 
   return (
-      <main className="TodoApp">
-        <div className="row">
+    <main className="TodoApp">
+      <div className="row">
 
-          <div className="col-md-6">
-            {todos.length > 0 &&
-              <EditableTodoList
-                todos={todos}
-                update={update}
-                remove={remove}
-              />
-            }
-            {todos.length === 0 &&
-              <span
-                className="text-muted">
-                  You have no todos.
-              </span>
-            }
-          </div>
-
-          <div className="col-md-6">
-            (if no top todo, omit this whole section)
-            {todos.length > 0 &&
-              <section className="mb-4">
-                <h3>Top Todo</h3>
-                <TopTodo todos={todos}/>
-            </section>
-            }
-
-            <section>
-              <h3 className="mb-3">Add Nü</h3>
-              <TodoForm handleSave={create}/>
-            </section>
-          </div>
-
+        <div className="col-md-6">
+          {todos.length > 0 &&
+            <EditableTodoList
+              todos={todos}
+              update={update}
+              remove={remove}
+            />
+          }
+          {todos.length === 0 &&
+            <span
+              className="text-muted">
+              You have no todos.
+            </span>
+          }
         </div>
-      </main>
+
+        <div className="col-md-6">
+          (if no top todo, omit this whole section)
+          {todos.length > 0 &&
+            <section className="mb-4">
+              <h3>Top Todo</h3>
+              <TopTodo todos={todos} />
+            </section>
+          }
+
+          <section>
+            <h3 className="mb-3">Add Nü</h3>
+            <TodoForm handleSave={create} />
+          </section>
+        </div>
+
+      </div>
+    </main>
   );
 }
 
