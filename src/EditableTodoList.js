6c4ed2bf58cd7b653e31a98c5abaf0f4c9ca2ffe
Nathan Rydel <nathanrydel@gmail.com>
2024-01-19 10:55:18 -0700

    updated docstring, ran autoformatter

--- src/EditableTodoList.js
+++ src/EditableTodoList.js
@@ -4,25 +4,25 @@ import EditableTodo from "./EditableTodo";
 /** Show list of editable todos.
  *
  * Props:
- * - todos: array of [ todo, ... ]
+ * - todos: Todo[] - [ {Todo}, {Todo}, ... ]
  * - update(): fn to call to update a todo
  * - remove(): fn to call to remove a todo
  *
  * TodoApp -> EditableTodoList -> [ EditableTodo, ... ]
  */
 
-function EditableTodoList({todos, update, remove }) {
-  console.log("EditableTodoList renders")
+function EditableTodoList({ todos, update, remove }) {
+  console.log("EditableTodoList renders", todos);
 
   return (
-      todos.map(todo => (
-        <EditableTodo
-          key={todo.id}
-          todo={todo}
-          update={update}
-          remove={remove}
-        />
-      ))
+    todos.map(todo => (
+      <EditableTodo
+        key={todo.id}
+        todo={todo}
+        update={update}
+        remove={remove}
+      />
+    ))
   );
 }
 
