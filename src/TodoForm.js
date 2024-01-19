08d6a167646fbe6b8d00bf9117520115be732353
Nathan Rydel <nathanrydel@gmail.com>
2024-01-18 17:35:00 -0700

    updated TodoForm logic and fixed values in JSX for each input field

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Todo from "./Todo";
 
 
 /** Form for adding.
@@ -7,66 +8,81 @@ import React, { useState } from "react";
  * - initialFormData
  * - handleSave: function to call in parent.
  *
+ * State:
+ * - formData
+ *
  * { TodoApp, EditableTodo } -> TodoForm
  */
 
-function TodoForm() {
+function TodoForm({ initialFormData, handleSave }) {
+  const [formData, setFormData] = useState(initialFormData);
 
   /** Update form input. */
-  function handleChange(evt) { }
+  // TODO: handle the formData being a string, but priority expecting a number
+  function handleChange(evt) {
+    const change = evt.target;
+    setFormData(fData => ({
+      ...fData,
+      [change.name]: change.value
+    }));
+  }
 
   /** Call parent function and clear form. */
-  function handleSubmit(evt) { }
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    handleSave(formData);
+    setFormData({ title: "", description: "", priority: 1 });
+  }
 
   return (
-      <form className="NewTodoForm" onSubmit={handleSubmit}>
+    <form className="NewTodoForm" onSubmit={handleSubmit}>
 
-        <div className="mb-3">
-          <input
-              id="newTodo-title"
-              name="title"
-              className="form-control"
-              placeholder="Title"
-              onChange={handleChange}
-              value="FIXME"
-              aria-label="Title"
-          />
+      <div className="mb-3">
+        <input
+          id="newTodo-title"
+          name="title"
+          className="form-control"
+          placeholder="Title"
+          onChange={handleChange}
+          value={formData.title}
+          aria-label="Title"
+        />
+      </div>
+
+      <div className="mb-3">
+        <textarea
+          id="newTodo-description"
+          name="description"
+          className="form-control"
+          placeholder="Description"
+          onChange={handleChange}
+          value={formData.description}
+          aria-label="Description"
+        />
+      </div>
+
+      <div className="mb-3 d-flex justify-content-between">
+        <div className="w-75 d-flex justify-content-between">
+          <label htmlFor="newTodo-priority"
+            className="d-inline-flex">Priority:&nbsp;&nbsp;
+          </label>
+          <select id="newTodo-priority"
+            name="priority"
+            value={formData.priority}
+            onChange={handleChange}
+            className="form-control form-control-sm d-inline-flex"
+          >
+            <option value={1}>Ultra-Über</option>
+            <option value={2}>Über</option>
+            <option value={3}>Meh</option>
+          </select>
         </div>
+        <button className="btn-primary rig btn btn-sm NewTodoForm-addBtn">
+          Gø!
+        </button>
+      </div>
 
-        <div className="mb-3">
-          <textarea
-              id="newTodo-description"
-              name="description"
-              className="form-control"
-              placeholder="Description"
-              onChange={handleChange}
-              value="FIXME"
-              aria-label="Description"
-          />
-        </div>
-
-        <div className="mb-3 d-flex justify-content-between">
-          <div className="w-75 d-flex justify-content-between">
-            <label htmlFor="newTodo-priority"
-                   className="d-inline-flex">Priority:&nbsp;&nbsp;
-            </label>
-            <select id="newTodo-priority"
-                    name="priority"
-                    value="FIXME"
-                    onChange={handleChange}
-                    className="form-control form-control-sm d-inline-flex"
-            >
-              <option value={1}>Ultra-Über</option>
-              <option value={2}>Über</option>
-              <option value={3}>Meh</option>
-            </select>
-          </div>
-          <button className="btn-primary rig btn btn-sm NewTodoForm-addBtn">
-            Gø!
-          </button>
-        </div>
-
-      </form>
+    </form>
   );
 }
 
