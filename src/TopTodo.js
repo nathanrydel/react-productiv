1a9b68ee3b7f5ab7dc4594ee94ec5b95c1e46f98
Nathan Rydel <nathanrydel@gmail.com>
2024-01-18 18:59:03 -0700

    added debug console log for render check

--- src/TopTodo.js
+++ src/TopTodo.js
@@ -11,6 +11,8 @@ import Todo from "./Todo";
  */
 
 function TopTodo({ todos }) {
+  console.log("TopTodo renders: ", todos)
+
   // lowest-priority # is the highest priority
   const top = todos.reduce(
     (acc, cur) => cur.priority < acc.priority ? cur : acc, todos[0]);
